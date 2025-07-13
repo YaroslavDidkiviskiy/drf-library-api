94629812ff51542fccd7beb181f9f5fb397ae1dc
Yaroslav Didkivskiy <yaroslavdidkivskiy@gmail.com>
2025-07-13 19:41:02 +0300

    logic fix for Borrowing model

--- library/models.py
+++ library/models.py
@@ -19,7 +19,7 @@ class Book(models.Model):
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField()
+    borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField()
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
